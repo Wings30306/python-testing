3551a2379ec0597f67558fd6c396c4189d04bb7d
Joke Heyndels <jo_hannah@outlook.com>
2021-08-05 07:51:28 +0000

    Lifecycle of a test

--- test_student.py
+++ test_student.py
@@ -4,18 +4,33 @@ from student import Student
 
 class TestStudent(unittest.TestCase):
 
+    # @classmethod
+    # def setUpClass(cls):
+    #     print("setUpClass method")
+
+    # @classmethod
+    # def tearDownClass(cls):
+    #     print("tearDownClass method")
+
+    def setUp(self):
+        # print("setUp method")
+        self.student = Student("John", "Doe")
+
+    # def tearDown(self):
+    #     print("tearDown method")
+
     def test_full_name(self):
-        student = Student("Johann", "Alberts")
-        self.assertEqual(student.full_name, "Johann Alberts")
+        # print("test_full_name")
+        self.assertEqual(self.student.full_name, "John Doe")
 
     def test_alert_santa(self):
-        student = Student("Draco", "Malfoy")
-        student.alert_santa()
-        self.assertTrue(student.naughty_list)
+        # print("test_alert_santa")
+        self.student.alert_santa()
+        self.assertTrue(self.student.naughty_list)
 
     def test_student_email(self):
-        student = Student("Harry", "Potter")
-        self.assertEqual(student.email(), "harry.potter@hogwarts.com")
+        # print("test_student_email")
+        self.assertEqual(self.student.email, "john.doe@hogwarts.com")
 
 
 if __name__ == "__main__":
